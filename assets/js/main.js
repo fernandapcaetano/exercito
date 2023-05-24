340181fc2e92b6e44909f82b2e4bb92ff61c4c30
Nanda P Caetano <fernandamariacaetanopereira@gmail.com>
2023-05-24 17:47:30 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,14 +5,36 @@ let erro = " ";
 
 function form() {
     var valueCPF = document.querySelector("input#cpf").value;
+    
     validarCPF(valueCPF);
+    if (validarCPF(valueCPF) == false){
+        erro_form.innerText = "Atenção: " + erro;
+    }
+    else{
+        erro_form.innerText = " ";
+    }
 
 }
 
+const inputCPF = document.querySelector("input#cpf");
+inputCPF.addEventListener('keypress', mascaraCPF); 
+function mascaraCPF() {
+    let escritaCPF = inputCPF.value.length;
+
+    if (escritaCPF === 3 || escritaCPF === 7){
+        inputCPF.value += '.'
+    }
+    else if (escritaCPF === 11){
+        inputCPF.value += '-';
+    }
+
+}
 function validarCPF(valueCPF) {
+
         valueCPF = valueCPF.replace(/[^\d]+/g,'');
         if (valueCPF === ""){
             erro = "Insira o CPF";
+            return false;
         }
         else if (valueCPF.length != 11 ||
             valueCPF == "00000000000" ||
@@ -26,19 +48,43 @@ function validarCPF(valueCPF) {
             valueCPF == "88888888888" ||
             valueCPF == "99999999999"){
             erro = "CPF Inválido";
+            return false
         }
-        erro_form.innerText = "Atenção: " + erro;
         
-        soma = 0;
-        dig1 = 0;
-        modulo = 0;
-        for (let i = 0; i < 9; i++) {
-            soma += valueCPF[i] * (10-i);
-            dig1 = soma;
-            modulo = (dig1 * 10) % 11;
-            console.log ("Módulo : " + modulo)  
+
+        let soma1 = 0;
+        for (var i = 0; i < 9; i++){
+            soma1 += valueCPF[i] * (10-i);
         }
+        soma1 = (soma1 * 10) % 11;
+        console.log("Primeiro: " + soma1);
+        if (soma1 == 10 || soma1 == 11){
+            soma1 = 0;
+        }
+        if (soma1 != valueCPF[9]) {
+            erro = "CPF inválido, digite novamente."
+            return false;
+        }
+
+        let soma2 = 0;
+        for (var i = 0; i < 10; i++){
+            soma2 += valueCPF[i] * (11-i);
+        }
+        soma2 = (soma2 * 10) % 11;
+        console.log("segundo "+ soma2);
+        if (soma2 == 10 || soma2 == 11){
+            soma2 = 0;
+        }
+        if (soma2 != valueCPF[10]) {
+            erro = "CPF inválido, digite novamente."
+            return false;
+        }
+        
+        erro_form.innerText = erro;
+    
+
 }
+
 
 
 function dataNascimento() {
